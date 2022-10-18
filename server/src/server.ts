aa5486429d44396f5897c81a47f3d95f2776d322
dev.jun28 <hsj9360@gmail.com>
2022-10-18 22:23:50 +0900

    feat(server): add votes route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,7 +3,8 @@ import morgan from "morgan";
 import { AppDataSource } from "./data-source";
 import authRoutes from "./routes/auth";
 import subRoutes from "./routes/subs";
-import postRoutes from "./routes/post";
+import postRoutes from "./routes/posts";
+import voteRoutes from "./routes/votes";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
@@ -25,6 +26,7 @@ app.get("/", (_, res) => res.send('running'));
 app.use("/api/auth", authRoutes);
 app.use("/api/subs", subRoutes);
 app.use('/api/posts', postRoutes)
+app.use('/api/votes', voteRoutes)
 
 app.use(express.static('public'));
 
